1e96cfa23002fc5ebf571f081804993c5bd97653
Hiroyuki Tachibana <hiroism007@gmail.com>
2022-12-31 11:18:28 +0900

    WIP

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,14 @@
 mod hello_world;
-use axum::{routing::get, Router};
+mod mirror_body_json;
+mod mirror_body_string;
+use axum::{routing::get, routing::post, Router};
 use hello_world::hello_world;
+use mirror_body_json::mirror_body_json;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_routes() -> Router {
-    Router::new().route("/", get(hello_world))
+    Router::new()
+        .route("/", get(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
+        .route("/mirror_body_json", post(mirror_body_json))
 }
